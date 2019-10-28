9c5d53bae34f8e00efe8d9c6b59652ab15576750
Phillip Hoff <phillip@orst.edu>
2019-10-28 14:06:02 -0700

    Integrated Material UI.

--- src/dapr-cards-web/pages/login.tsx
+++ src/dapr-cards-web/pages/login.tsx
@@ -1,11 +1,15 @@
-import React from 'react'
-import Head from 'next/head'
+import React from 'react';
+import Head from 'next/head';
+import Button from '@material-ui/core/Button';
 
 const Login = () => (
     <div>
         <Head>
             <title>Login</title>
         </Head>
+        <Button variant="contained" color="primary">
+            Login
+        </Button>
     </div>
 );
 

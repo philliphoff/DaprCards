fe419bdb885e3d280a02afead1524d5e85e2507b
Phillip Hoff <phillip@orst.edu>
2019-10-28 13:54:00 -0700

    Sketch Next.js based web app.

---
+++ src/dapr-cards-web/pages/login.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Head from 'next/head'
+
+const Login = () => (
+    <div>
+        <Head>
+            <title>Login</title>
+        </Head>
+    </div>
+);
+
+export default Login;

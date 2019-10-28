03269d1ffdfb843f8dbde6ad1afc7a33e37002b6
Phillip Hoff <phillip@orst.edu>
2019-10-28 16:16:34 -0700

    Sketch front end API to DAPR services.

---
+++ src/dapr-cards-web/pages/api/users.ts
@@ -0,0 +1,25 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import * as http from 'http';
+import * as request from 'request-promise-native';
+
+const daprPort = process.env.DAPR_HTTP_PORT || 3500;
+const daprUrl = `http://localhost:${daprPort}/v1.0/invoke`;
+
+const daprInvocationUrl = (appId: string, methodName: string) => `${daprUrl}/${appId}/method/${methodName}`;
+const getUsersUrl = () => daprInvocationUrl('dapr-user-manager', 'users');
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+    var getUsersResponse = await request.get(
+        getUsersUrl(),
+        {
+            headers: {
+                'Accept': 'application/json'
+            },
+            json: false
+        }
+    );
+
+    res.setHeader('Content-Type', 'application/json');
+    res.statusCode = 200;
+    res.end(getUsersResponse);
+};
--- src/dapr-cards-web/pages/login.tsx
+++ src/dapr-cards-web/pages/login.tsx
@@ -1,16 +1,59 @@
 import React from 'react';
 import Head from 'next/head';
 import Button from '@material-ui/core/Button';
+import Grid from '@material-ui/core/Grid';
+import { makeStyles } from '@material-ui/styles';
+import Paper from '@material-ui/core/Paper';
+import TextField from '@material-ui/core/TextField';
+import Typography from '@material-ui/core/Typography';
 
-const Login = () => (
-    <div>
-        <Head>
-            <title>Login</title>
-        </Head>
-        <Button variant="contained" color="primary">
-            Login
-        </Button>
-    </div>
-);
+const useStyles = makeStyles(theme => ({
+    grid: {
+        justify: 'center'
+    },
+    paper: {
+        width: 500
+    },
+    signInRoot: {
+        display: 'flex',
+        flexDirection: 'column'
+    }
+}));
+
+const Login = (props) => {
+    const classes = useStyles(props);
+
+    return (
+        <div>
+            <Head>
+                <title>Login</title>
+            </Head>
+            <Grid container className={classes.grid} justify="center" spacing={2}>
+                <Grid item>
+                    <Paper className={classes.paper}>
+                        <div>
+                            <Typography>First time here? Sign up!</Typography>
+                            <Button variant="contained" color="primary">
+                                Sign up
+                            </Button>
+                        </div>
+                    </Paper>
+                </Grid>
+                <Grid item>
+                    <Paper className={classes.paper}>
+                        <div className={classes.signInRoot}>
+                            <Typography>Already a gamer? Sign in!</Typography>
+                            <TextField label="Email" />
+                            <TextField label="Password" />
+                            <Button variant="contained" color="primary">
+                                Sign in
+                            </Button>
+                        </div>
+                    </Paper>
+                </Grid>
+            </Grid>
+        </div>
+    );
+};
 
 export default Login;
